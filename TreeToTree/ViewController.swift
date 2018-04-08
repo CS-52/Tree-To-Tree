b2eee43a36a349300e4674a46c54d45f2e0e65a7
Jose Giron <jmgiron@stanford.edu>
2018-04-08 14:47:20 -0700

    Initial Commit

---
+++ TreeToTree/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TreeToTree
+//
+//  Created by Jose Miguel Giron on 8/4/18.
+//  Copyright © 2018 CS+Social Good. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TreeToTreeTests/TreeToTreeTests.swift
@@ -0,0 +1,36 @@
+//
+//  TreeToTreeTests.swift
+//  TreeToTreeTests
+//
+//  Created by Jose Miguel Giron on 8/4/18.
+//  Copyright © 2018 CS+Social Good. All rights reserved.
+//
+
+import XCTest
+@testable import TreeToTree
+
+class TreeToTreeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
