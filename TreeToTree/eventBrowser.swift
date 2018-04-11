da96ede7e2569b51ceead660d3f9b4b69124b2db
Suzanne Joh <johsu@users.noreply.github.com>
2018-04-10 20:00:53 -0700

    events browser init

---
+++ TreeToTree/eventBrowser.swift
@@ -0,0 +1,26 @@
+//
+//  eventBrowser.swift
+//  TreeToTree
+//
+//  Created by Suzanne Joh on 4/10/18.
+//  Copyright © 2018 CS+Social Good. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class EventBrowser: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBOutlet weak var eventList: UITableView!
+}
