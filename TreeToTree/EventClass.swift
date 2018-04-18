9188006beacc91fb6dbb0c75870e11016236b6a1
Austin Chow <archow@stanford.edu>
2018-04-17 19:27:00 -0700

    event class init

---
+++ TreeToTree/EventClass.swift
@@ -0,0 +1,21 @@
+//
+//  EventClass.swift
+//  TreeToTree
+//
+//  Created by Austin Chow on 4/17/18.
+//  Copyright © 2018 CS+Social Good. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class EventClass {
+    static var name: String!
+    static var date: String!
+    static var shifts: String!
+    static var image: UIImage!
+    static var people: String!
+    static var interest: Int!
+    
+}
+
