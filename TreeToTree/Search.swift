d0b0a7edf559cdef089e52ca1123fc5de48bac27
Suzanne Joh <johsu@users.noreply.github.com>
2018-04-24 19:42:34 -0700

    search bar back to prev functionality

--- TreeToTree/Search.swift
+++ TreeToTree/Search.swift
@@ -6,25 +6,25 @@
 //  Copyright © 2018 CS+Social Good. All rights reserved.
 //
 
-import UIKit
-
-class Search: UIViewController, UISearchBarDelegate {
-    
-    var searchArray = [String]()
-    
-    @IBOutlet weak var searchBar: UISearchBar!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-  
-        
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    
-}
+//import UIKit
+//
+//class Search: UIViewController, UISearchBarDelegate {
+//
+//    var searchArray = [String]()
+//
+//    @IBOutlet weak var searchBar: UISearchBar!
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+//
+//
+//
+//}
 
