f98a9208b21f7d563c2e54ca7d837c551fea6bec
Austin Chow <archow@stanford.edu>
2018-04-26 19:44:08 -0700

    groupCell

---
+++ TreeToTree/GroupPageCell.swift
@@ -0,0 +1,15 @@
+//
+//  GroupPageCell.swift
+//  TreeToTree
+//
+//  Created by Austin Chow on 4/26/18.
+//  Copyright © 2018 CS+Social Good. All rights reserved.
+//
+
+import UIKit
+
+class GroupPageCell: UITableViewCell {
+
+
+}
+
---
+++ TreeToTree/GroupPageViewController.swift
@@ -0,0 +1,28 @@
+//
+//  GroupPageViewController.swift
+//  TreeToTree
+//
+//  Created by Austin Chow on 4/26/18.
+//  Copyright © 2018 CS+Social Good. All rights reserved.
+//
+
+import UIKit
+
+class GroupPageViewController: UIViewController {
+    
+   
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+       
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
