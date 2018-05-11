07920add6e21324d69e1974306b950bd3b2b358f
Austin Chow <archow@stanford.edu>
2018-05-10 19:31:11 -0700

    groupTableView added

--- TreeToTree/GroupPageCell.swift
+++ TreeToTree/GroupPageCell.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class GroupPageCell: UITableViewCell {
 
-
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var groupPhoto: UIImageView!
+    @IBOutlet weak var groupName: UILabel!
+    @IBAction func onGroupClick(_ sender: Any) {
+        let button = sender as! UIButton;
+        if((button.image(for: .normal)) == #imageLiteral(resourceName: "Oval")) {
+            button.setImage(#imageLiteral(resourceName: "Oval-1"), for: .normal)
+        } else {
+            button.setImage(#imageLiteral(resourceName: "Oval"), for: .normal)
+        }
+        
+    }
 }
 
--- TreeToTree/GroupPageViewController.swift
+++ TreeToTree/GroupPageViewController.swift
@@ -8,19 +8,54 @@
 
 import UIKit
 
-class GroupPageViewController: UIViewController {
+class GroupPageViewController: UITableViewController {
     
-   
+    var groupsArray = [Person]()
+
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        loadSampleGroups()
        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return groupsArray.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "GroupCell"
+
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? GroupPageCell  else {
+            fatalError("The dequeued cell is not an instance of GroupCell.")
+        }
+        
+        let group = groupsArray[indexPath.row]
+        cell.groupPhoto.image = group.image
+        cell.groupName.text = group.name
+        
+        return cell
+    }
+    
+    private func loadSampleGroups() {
+        //let photo = UIImage(named: "austinchow")
+        let person1 = Person(name: "Austin Chow", image: #imageLiteral(resourceName: "austinchow"))
+        let person2 = Person(name: "Suzanne Joh", image: #imageLiteral(resourceName: "suzannejoh"))
+        let person3 = Person(name: "Keith Schwartz", image: #imageLiteral(resourceName: "afriendlyface"))
+        let person4 = Person(name: "Chris Piech", image: #imageLiteral(resourceName: "piech"))
+        let person5 = Person(name: "Mehran Sahami", image: #imageLiteral(resourceName: "Mehran"))
+        
+        groupsArray += [person1, person2, person3, person4, person5]
+        
+        
     }
     
     
