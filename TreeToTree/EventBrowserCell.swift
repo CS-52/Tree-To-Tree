a529b28d62e9c3926ea5c35c0ea6498408f72d00
Suzanne Joh <johsu@users.noreply.github.com>
2018-05-25 15:53:01 -0700

    comments for heart button

--- TreeToTree/EventBrowserCell.swift
+++ TreeToTree/EventBrowserCell.swift
@@ -23,9 +23,15 @@ class EventBrowserCell: UITableViewCell {
         let button = sender as! UIButton;
         if((button.image(for: .normal)) == #imageLiteral(resourceName: "heartOutline")) {
             button.setImage(#imageLiteral(resourceName: "heart"), for: .normal)
+            //add this event to the user's list of interested ids and add this user to the event's list of interested ids
+            //This will trigger a function in the back end
+            
         } else {
             button.setImage(#imageLiteral(resourceName: "heartOutline"), for: .normal)
+            //remove this event from user, remove user from event
         }
+        
+        //show up as filled for events already in user's list
         
     }
     
