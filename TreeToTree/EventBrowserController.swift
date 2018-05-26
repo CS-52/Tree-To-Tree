47e2840b1497c335c4cdbbd96ed3ecdbd12fd879
Suzanne Joh <johsu@users.noreply.github.com>
2018-05-26 14:05:43 -0700

    Tried to make shifts, not sure how

--- TreeToTree/EventBrowserController.swift
+++ TreeToTree/EventBrowserController.swift
@@ -105,8 +105,7 @@ class EventBrowserController: UIViewController, UITableViewDelegate, UITableView
         cell.eventName.text = event.name
         cell.shifts.text = "\(event.shifts.count) shifts available."
         cell.peopleGoing.text = "\(event.goingIDs.count) people going."
-        //cell.interest.interest = event.interest
-  
+    
         
         return cell
         
--- TreeToTree/EventPageViewController.swift
+++ TreeToTree/EventPageViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class EventPageViewController: UIViewController{
+class EventPageViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var eventSummary: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
@@ -24,13 +24,17 @@ class EventPageViewController: UIViewController{
     @IBOutlet weak var friend4: UIImageView!
     @IBOutlet weak var friend5: UIImageView!
     
+    @IBOutlet weak var shiftsTableView: UITableView!
+    
     //var arrayElem:String?
     var arrayElem:Event?
     
+    var shifts = [String : Dictionary<String, [String]>](){didSet{shiftsTableView.reloadData()}}
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        shiftsTableView.delegate = self
+        shiftsTableView.dataSource = self
         
         nameLabel.text = arrayElem?.name
         dateLabel.text = arrayElem?.date
@@ -46,7 +50,43 @@ class EventPageViewController: UIViewController{
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (arrayElem?.shifts)!.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "ShiftCell"
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? ShiftCell  else {
+            fatalError("The dequeued cell is not an instance of ShiftCell.")
+        }
+        //COME BACK LATER -SUZANNE
+//        self.shifts = (arrayElem?.shifts)!
+//        cell.nameLabel.text = shifts["0000"]?["name"]
+//        cell.timeLabel.text = (shifts["0000"]?["start time"])! + "-" + (shifts["0000"]?["end time"])!
+//
+        
+//        for shift in (arrayElem?.shifts)! {
+//            self.shifts += shift;
+//        }
+//
+//        for (id, shift) in shifts {
+//
+//            cell.nameLabel.text = shift[id]?["name"]
+//            cell.timeLabel.text = (shift[id]?["start time"])! + "-" + (shift[id]?["end time"])!
+//            //return cell
+//
+//        }
+//
+//        let shift = shifts[indexPath.row]
+        
+        
+        return cell
+        
+        
+    }
+    
     
 }
 
